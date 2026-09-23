db79a225d247c9efee31cf34c7b7da0a939a92fd
ZacharyKinslow <w0293731@campus.nscc.ca>
2026-09-23 18:15:34 -0300

    Created Movie model for check-in 3

---
+++ MovieAdmin/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿namespace MovieAdmin.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Genre { get; set; } = string.Empty;
+
+        public int RunTime { get; set; }
+
+        public DateTime ReleaseDate { get; set; } //First put string then forgot i learned about DateTime
+
+        public bool NewRelease { get; set; } // First put string AGAIN but remembered its only a true or false so bool was the correct type
+
+        public float Rating { get; set; } //First put int but didnt consider ratings being outside of whole numbers so changed to float
+
+
+    }
+}
